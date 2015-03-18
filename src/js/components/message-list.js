f22e1c6871e32701225141bceda4281fbe0bc1d7
Giles Van Gruisen <gilesvangruisen@me.com>
2015-03-18 18:11:13 +1100

    message-list: use props.messages for messages

--- src/js/components/message-list.js
+++ src/js/components/message-list.js
@@ -1,44 +1,20 @@
 import React from 'react';
 import { addons } from 'react/addons';
-import ChannelStore from '../stores/channel-store';
 
 const component = React.createClass({
   mixins: [addons.PureRenderMixin],
-
-  componentWillMount() {
-    ChannelStore.addChangeListener(this._onChange);
-  },
-
-  getInitialState() {
-    return {
-      channel: null,
-      messages: null
-    };
-  },
-
-  _onChange() {
-    const channel = ChannelStore.getSelectedChannel();
-    this.setState({
-      channel: channel,
-      messages: channel.getMessages()
-    });
-  },
 
   scrollToBottom() {
     const el = React.findDOMNode(this.refs.scroller);
     el.scrollTop = el.scrollHeight;
   },
 
-  componentDidUpdate() {
-    this.scrollToBottom();
-  },
-
   render() {
-    const { channel, messages } = this.state;
+    const messages = this.props.messages;
 
-    if (!channel) {
+    if (!messages) {
       return null;
-    }
+    };
 
     const messageElements = messages.map((msg, i) => {
       const action = (msg.type !== 'priv') ? <span className="action">{msg.type}</span> : '';
