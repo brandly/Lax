2264f4167047e7dedfb5d62ca1030f5fb0a984ff
Giles Van Gruisen <gilesvangruisen@me.com>
2015-03-18 18:11:52 +1100

    channel-header: use props.channel for channel name, props.people for people count

--- src/js/components/channel-header.js
+++ src/js/components/channel-header.js
@@ -1,37 +1,23 @@
 import React from 'react';
 import { addons } from 'react/addons';
-import { List } from 'immutable';
-import ChannelStore from '../stores/channel-store';
 
 const component = React.createClass({
   mixins: [addons.PureRenderMixin],
 
-  componentWillMount() {
-    ChannelStore.addChangeListener(this._onChange);
-  },
-
-  getInitialState() {
-    return {
-      channelName: '',
-      channelPeople: List()
-    };
-  },
-
-  _onChange() {
-    const channel = ChannelStore.getSelectedChannel();
-    this.setState({
-      channelName: channel.name,
-      channelPeople: channel.getPeople()
-    });
-  },
-
   render() {
-    const peopleCount = this.state.channelPeople.size;
+    const channel = this.props.channel;
+    const peopleCount = this.props.people.size;
 
-    if (this.state.channelName) {
+    if (!channel) {
+      return null;
+    }
+
+    const channelName = channel.name;
+
+    if (channelName) {
       return (
         <div className="channel-header">
-          <h2 className="channel-name">{this.state.channelName}</h2>
+          <h2 className="channel-name">{channelName}</h2>
           <p className="channel-people-count">{peopleCount} people</p>
         </div>
       );
