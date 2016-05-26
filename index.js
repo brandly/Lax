0e3e98db498ea226c7b4f977c39a3a99bc513970
brandly <brandly13@gmail.com>
2016-05-26 00:56:11 -0400

    make the window wider to start

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1000, height: 600})
 
   // and load the index.html of the app.
   win.loadURL(path.join(`file://${__dirname}`, `${pkg['main-html']}`))
