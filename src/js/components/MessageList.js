27a3b978ce724105a0fa15ad4b629af11e703da6
brandly <brandly13@gmail.com>
2018-02-20 16:14:05 -0500

    leverage PureComponent to improve MessageList performance, type check MessageList

--- src/js/components/MessageList.js
+++ src/js/components/MessageList.js
@@ -1,3 +1,5 @@
+// @flow
+/* global HTMLDivElement HTMLAnchorElement */
 import React from 'react'
 import { findDOMNode } from 'react-dom'
 import classNames from 'classnames'
@@ -13,7 +15,8 @@ type State = {
   isBrowsingPriorMessages: boolean
 };
 
-class MessageList extends React.Component<Props, State> {
+class MessageList extends React.PureComponent<Props, State> {
+  scrollListener: EventHandler;
   constructor (props: Props) {
     super(props)
     this.state = {
@@ -25,37 +28,69 @@ class MessageList extends React.Component<Props, State> {
     const el = findDOMNode(this.refs.scroller)
 
     this.scrollListener = () => {
-      const isScrolledToBottom = el.scrollTop === (el.scrollHeight - el.offsetHeight)
-      this.setState({ isBrowsingPriorMessages: !isScrolledToBottom })
+      if (el instanceof HTMLDivElement) {
+        const isScrolledToBottom = el.scrollTop === (el.scrollHeight - el.offsetHeight)
+        this.setState({ isBrowsingPriorMessages: !isScrolledToBottom })
+      }
     }
 
-    el.addEventListener('scroll', this.scrollListener)
+    el && el.addEventListener('scroll', this.scrollListener)
   }
 
   componentWillUnmount () {
     const el = findDOMNode(this.refs.scroller)
-    el.removeEventListener('scroll', this.scrollListener)
+    el && el.removeEventListener('scroll', this.scrollListener)
   }
 
   scrollToBottom () {
     const el = findDOMNode(this.refs.scroller)
-    el.scrollTop = el.scrollHeight
+    if (el instanceof HTMLDivElement) {
+      el.scrollTop = el.scrollHeight
+    }
   }
 
-  componentDidUpdate (nextProps) {
+  componentDidUpdate (nextProps: Props) {
     const isDifferentChannel = nextProps.messages[0] !== this.props.messages[0]
     if (isDifferentChannel || !this.state.isBrowsingPriorMessages) {
       this.scrollToBottom()
     }
   }
 
-  handleLink (event) {
+  render () {
+    const { messages } = this.props
+
+    if (!messages) {
+      return null
+    }
+
+    return (
+      <div className="scrolling-panel" ref="scroller">
+        <ul className="message-list">
+          {messages.map((msg, i) => {
+            const showFrom = (i === 0 || messages[i - 1].from !== msg.from)
+
+            return <Message key={msg.id} msg={msg} showFrom={showFrom} />
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+type MessageProps = {
+  msg: MessageT,
+  showFrom: boolean
+};
+class Message extends React.PureComponent<MessageProps> {
+  handleLink (event: Event) {
     event.preventDefault()
-    const url = event.target.href
-    shell.openExternal(url)
+    if (event.target instanceof HTMLAnchorElement) {
+      const url = event.target.href
+      shell.openExternal(url)
+    }
   }
 
-  renderMessage (text: string) {
+  renderText (text: string) {
     return text.split('\n').map((text, index) => {
       // TODO: should probably handle this with CSS
       const Wrap = ({ children }) => {
@@ -74,36 +109,20 @@ class MessageList extends React.Component<Props, State> {
   }
 
   render () {
-    const { messages } = this.props
+    const { msg, showFrom } = this.props
+    const action = (msg.type !== 'priv') ? <span className="command">{msg.type}</span> : null
 
-    if (!messages) {
-      return null
-    }
-
-    const messageElements = messages.map((msg, i) => {
-      const action = (msg.type !== 'priv') ? <span className="command">{msg.type}</span> : null
-      const showFrom = (i === 0 || messages[i - 1].from !== msg.from)
-
-      const classes = classNames({
-        message: true,
-        [msg.type]: true
-      })
-
-      return (
-        <li className={classes} key={msg.id}>
-          <h3 className="nickname from">{showFrom ? msg.from : ''}</h3>
-          <div className="body">{action}{this.renderMessage(msg.text)}</div>
-          <p className="when">{formatDate(msg.when)}</p>
-        </li>
-      )
+    const classes = classNames({
+      message: true,
+      [msg.type]: true
     })
 
     return (
-      <div className="scrolling-panel" ref="scroller">
-        <ul className="message-list">
-          {messageElements}
-        </ul>
-      </div>
+      <li className={classes}>
+        <h3 className="nickname from">{showFrom ? msg.from : ''}</h3>
+        <div className="body">{action}{this.renderText(msg.text)}</div>
+        <p className="when">{formatDate(msg.when)}</p>
+      </li>
     )
   }
 }
