7e20f2a307ad09bcfad093d3bc9bc8267996f595
brandly <brandly13@gmail.com>
2018-03-14 10:38:52 -0400

    add application menu, fixes #26

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const electron = require('electron')
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, Menu } = electron
 const pkg = require('./package')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -37,6 +37,28 @@ function createWindow () {
     // Open the DevTools.
     win.webContents.openDevTools()
   }
+
+  const template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit() }}
+    ]
+  }, {
+    label: 'Edit',
+    submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ]}
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 }
 
 // This method will be called when Electron has finished
