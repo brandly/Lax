c88313fe892f6ab35e9c7653c37f1e4bcb75fba4
brandly <brandly13@gmail.com>
2019-08-28 00:35:57 -0400

    sketch out suggestions logic

--- src/js/components/ComposeMessage.js
+++ src/js/components/ComposeMessage.js
@@ -1,21 +1,40 @@
 // @flow
-/* global SyntheticEvent, HTMLInputElement */
+/* global SyntheticEvent, KeyboardEvent, HTMLInputElement */
 import React from 'react'
+import SelectList from '../modules/SelectList'
 
 type Props = {
   nickname: string,
   onMessage: string => void
 }
 
+type SuggestionsType = ?SelectList<string>
 type State = {
-  message: string
+  message: string,
+  suggestions: SuggestionsType,
+  isFocused: boolean
+}
+
+const possibleSuggestions = new SelectList([], '/msg', [
+  '/me',
+  '/join',
+  '/part',
+  '/nick'
+])
+const suggestionsFor = (msg: string): SuggestionsType => {
+  if (msg.length) {
+    return possibleSuggestions.filter(suggestion => suggestion.startsWith(msg))
+  }
+  return null
 }
 
 class ComposeMessage extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
     this.state = {
-      message: ''
+      message: '',
+      suggestions: suggestionsFor(''),
+      isFocused: false
     }
   }
 
@@ -26,7 +45,8 @@ class ComposeMessage extends React.Component<Props, State> {
   }
 
   setMessage(message: string) {
-    this.setState({ message })
+    const suggestions = suggestionsFor(message)
+    this.setState({ message, suggestions })
   }
 
   handleChange(event: SyntheticEvent<*>) {
@@ -35,8 +55,38 @@ class ComposeMessage extends React.Component<Props, State> {
     }
   }
 
+  handleKeyDown(event: KeyboardEvent) {
+    if (this.state.suggestions) {
+      switch (event.key) {
+        case 'ArrowUp': {
+          this.setState({
+            suggestions: this.state.suggestions.prevWrap()
+          })
+          event.preventDefault()
+          break
+        }
+        case 'ArrowDown': {
+          this.setState({
+            suggestions: this.state.suggestions.nextWrap()
+          })
+          event.preventDefault()
+          break
+        }
+        case 'ArrowRight': {
+          const message = this.state.suggestions.selected + ' '
+          this.setState({
+            message,
+            suggestions: suggestionsFor(message)
+          })
+          break
+        }
+      }
+    }
+  }
+
   render() {
     const { nickname } = this.props
+    const { suggestions, isFocused } = this.state
 
     return (
       <form
@@ -44,17 +94,47 @@ class ComposeMessage extends React.Component<Props, State> {
         onSubmit={this.handleFormSubmission.bind(this)}
       >
         <h3 className="nickname from">{nickname}</h3>
-        <input
-          type="text"
-          placeholder="write message"
-          className="body"
-          required
-          value={this.state.message}
-          onChange={this.handleChange.bind(this)}
-        />
+        <div style={{ height: '100%' }}>
+          {isFocused && suggestions ? <Suggestions list={suggestions} /> : null}
+          <input
+            type="text"
+            placeholder="write message"
+            className="body"
+            required
+            value={this.state.message}
+            onChange={this.handleChange.bind(this)}
+            onKeyDown={this.handleKeyDown.bind(this)}
+            onFocus={() => {
+              this.setState({
+                isFocused: true
+              })
+            }}
+            onBlur={() => {
+              this.setState({
+                isFocused: false
+              })
+            }}
+          />
+        </div>
       </form>
     )
   }
 }
+
+const Suggestions = props => (
+  <ul style={{ position: 'absolute', bottom: '100%' }}>
+    {props.list
+      .map((s, isSelected) => (
+        <li
+          key={s}
+          className="selected"
+          style={{ fontWeight: isSelected ? 'bold' : 'inherit' }}
+        >
+          {s}
+        </li>
+      ))
+      .toArray()}
+  </ul>
+)
 
 export default ComposeMessage
